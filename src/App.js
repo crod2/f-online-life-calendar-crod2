d1e05ee4ea125532cb2c36e2f8e5bbfbad58e044
crod2 <carlarodriguez90@gmail.com>
2019-06-29 19:13:13 +0200

    create webapp basic info

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      date: [],
+      smiley: []
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+
+        <div className="smiley__container">
+          <button className="btn__add">+</button>
+        </div>
+
+        <div className="input__container">
+
+          <div className="date__container">
+            <label htmlFor="date">Fecha </label>
+            <input type="text" name="date" id="date"/>
+          </div>
+
+          <div className="state__container">
+            <label htmlFor="state">Estado </label>
+
+            <input type="checkbox" name="checkbox" id="happy" value=":)"/>
+            <label htmlFor="happy">:)</label>
+
+            <input type="checkbox" name="checkbox" id="sad" value=":("/>
+            <label htmlFor="sad">:(</label>
+          </div>
+
+          <div className="buttons">
+            <button className="btn__save">guardar</button>
+            <button className="btn__cancel">cancelar</button>
+          </div>
+
+        </div>
+
+      </div>
+      
+
+
+    )
+  }
 }
 
 export default App;
